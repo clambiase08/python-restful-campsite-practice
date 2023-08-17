27b98d1706f12d8d48d36b98d425ce59cd7c3fc8
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-17 13:54:17 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -18,8 +18,63 @@ api = Api(app)
 
 
 class Parks(Resource):
-    pass
+    def get(self):
+        parks = [park.to_dict() for park in Park.query.all()]
+        return make_response(parks, 200)
+
+
+api.add_resource(Parks, "/parks")
+
+
+class ParkById(Resource):
+    def get(self, id):
+        park = Park.query.get_or_404(id).to_dict()
+        # park = Park.query.filter_by(id=id).first().to_dict()
+        return make_response(park, 200)
+
+    def patch(self, id):
+        data = request.get_json()
+        park = Park.query.get_or_404(id)
+        for attr, value in data.items():
+            setattr(park, attr, value)
+        db.session.add(park)
+        db.session.commit()
+        return make_response(park.to_dict(), 202)
+
+    def delete(self, id):
+        park = Park.query.get_or_404(id)
+        db.session.delete(park)
+        db.session.commit()
+        return make_response("", 204)
+
+
+api.add_resource(ParkById, "/parks/<int:id>")
+
+
+class Campsites(Resource):
+    def get(self):
+        campsites = [site.to_dict() for site in Campsite.query.all()]
+        return make_response(campsites, 200)
+
+    def post(self):
+        data = request.get_json()
+        # new_site = Campsite(
+        #     max_capacity=data["max_capacity"],
+        #     type=data["type"],
+        #     site_fee=data["site_fee"],
+        #     has_water=data["has_water"],
+        #     has_bathroom=data["has_bathroom"],
+        #     has_grill=data["has_grill"],
+        #     park_id=data["park_id"],
+        # )
+        new_site = Campsite(**data)
+        db.session.add(new_site)
+        db.session.commit()
+        return make_response(new_site.to_dict(), 201)
+
+
+api.add_resource(Campsites, "/campsites")
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,14 +6,85 @@ db = SQLAlchemy()
 
 
 class Park(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "parks"
+    serialize_rules = ("-campsites.park",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    address = db.Column(db.String, nullable=False)
+    entrance_fee = db.Column(db.Float)
+    has_trails = db.Column(db.Boolean)
+    has_RV_cleanout = db.Column(db.Boolean)
+    begin_camping_season = db.Column(db.DateTime)
+    end_camping_season = db.Column(db.DateTime)
+
+    campsites = db.relationship("Campsite", backref="park", cascade="delete")
+
+    @validates("entrance_fee")
+    def validate_fee(self, key, entrance_fee):
+        if not 13.99 < entrance_fee < 25:
+            raise ValueError("Fee must be between $13.99 and $25")
+        return entrance_fee
 
     def __repr__(self):
-        pass
+        return f"<Park: {self.name} | Address: {self.address}>"
 
 
 class Campsite(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "campsites"
+    serialize_rules = ("-reservations.campsite",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    max_capacity = db.Column(db.Integer)
+    type = db.Column(db.String)
+    site_fee = db.Column(db.Float)
+    has_water = db.Column(db.Boolean)
+    has_bathroom = db.Column(db.Boolean)
+    has_grill = db.Column(db.Boolean)
+    park_id = db.Column(db.Integer, db.ForeignKey("parks.id"))
+
+    reservations = db.relationship("Reservation", backref="campsite", cascade="delete")
+
+    @validates("max_capacity")
+    def validate_capacity(self, key, capacity):
+        if not capacity <= 10:
+            raise ValueError("Max Capacity must be less than or equal to 10")
+        return capacity
+
+    @validates("type")
+    def validate_type(self, key, type):
+        if type != "tent" and type != "RV":
+            raise ValueError("Invalid type")
+        return type
 
     def __repr__(self):
-        pass
+        return f"<Campsite ID: {self.id} | type: {self.type} | capacity: {self.max_capacity}"
+
+
+class Reservation(db.Model, SerializerMixin):
+    __table_name__ = "reservations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    campsite_id = db.Column(db.Integer, db.ForeignKey("campsites.id"))
+
+    @validates("start_date")
+    def validate_start(self, key, start_date):
+        if (
+            not self.campsite.park.begin_camping_season
+            < start_date
+            < self.campsite.park.end_camping_season
+        ):
+            raise ValueError("Start date must be in the camping season")
+        return start_date
+
+    @validates("end_date")
+    def validate_end(self, key, end_date):
+        if (
+            not self.campsite.park.begin_camping_season
+            < end_date
+            < self.campsite.park.end_camping_season
+        ):
+            raise ValueError("End date must be in the camping season")
+        return end_date
